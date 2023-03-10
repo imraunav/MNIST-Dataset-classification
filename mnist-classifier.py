d218b0fda778d4fe5e8055d5f146aeadb860c90f
imraunav <raun.ghosh@gmail.com>
2023-03-10 12:45:32 +0530

    This is local device update.

--- mnist-classifier.py
+++ mnist-classifier.py
@@ -1,5 +1,91 @@
-from matplotlib import pyplot as plt
-import tensorflow as tf
-# import tensorflow_datasets as tfds # will learn to use this in the future
-from tensorflow.keras.datasets import mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data(path='/Users/raunavghosh/Documents/Research for Vehant/MNIST/MNIST-Dataset-classification/mnist.npz')
+# from matplotlib import pyplot as plt
+# import tensorflow as tf
+
+# # import tensorflow_datasets as tfds # will learn to use this in the future
+# from tensorflow.keras.datasets import mnist
+# (train_images, train_labels), (test_images, test_labels) = mnist.load_data(path='/Users/raunavghosh/Documents/Research for Vehant/MNIST/MNIST-Dataset-classification/mnist.npz')
+
+# # Preprocessing
+# # normalizing all values
+# train_images = train_images/255.0
+# test_images = test_images/255.0
+
+# # convert all labels to one-hot notation
+# # trian_labels = tf.one_hot(train_labels, 10)
+# # test_labels = tf.one_hot(test_labels, 10)
+
+# # following the tutorial at: https://www.tensorflow.org/tutorials/images/cnn
+# from tensorflow.keras import models, layers
+
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+# # adding dense layers
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(10))
+
+# # model summary
+# # model.summary()
+
+# model.compile(optimizer='adam',
+#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#               metrics=['accuracy'])
+
+# history = model.fit(train_images, train_labels, epochs=10, 
+#                     validation_data=(test_images, test_labels))
+
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# Load the data and split it between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+
+model.summary()
+
+batch_size = 128
+epochs = 15
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
